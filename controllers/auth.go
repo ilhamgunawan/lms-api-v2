9091f55f8002ae1fcb9cbd3a09a296fa15b0114e
Ilham Muhammad Ramdhan Gunawan <ilhammrg@gmail.com>
2023-02-16 17:19:12 +0700

    Auth: login controller

---
+++ controllers/auth.go
@@ -0,0 +1,71 @@
+package controllers
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/ilhamgunawan/lms-api-v2/models"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type LoginRequest struct {
+	Username string
+	Password string
+}
+
+type LoginResponse struct {
+	Username  string `json:"user_name"`
+	UserID    string `json:"user_id"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Gender    string `json:"gender"`
+	BirthDate string `json:"date_of_birth"`
+	Token     string `json:"token"`
+}
+
+func Login(c *gin.Context) {
+	var body LoginRequest
+
+	err := c.ShouldBindJSON(&body)
+
+	if err != nil || body.Username == "" || body.Password == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"message": "Missing fields"})
+		return
+	}
+
+	user, err := models.GetUserByUsername(body.Username)
+
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"message": "Username or password not match"})
+		return
+	}
+
+	plainPassword := []byte(body.Password)
+	hashedPassword := []byte(user.PasswordHash)
+
+	err = bcrypt.CompareHashAndPassword(hashedPassword, plainPassword)
+
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"message": "Username or password not match"})
+		return
+	}
+
+	token, err := models.CreateToken(user)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"message": err})
+		return
+	}
+
+	res := LoginResponse{
+		Username:  user.Username,
+		UserID:    user.UserID,
+		FirstName: user.FirstName,
+		LastName:  user.LastName,
+		Gender:    user.Gender,
+		BirthDate: user.BirthDate,
+		Token:     token,
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": res})
+}
