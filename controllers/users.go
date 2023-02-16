36b3ba1050d6b5e0bd84d31b3299b0bf886bb3aa
Ilham Muhammad Ramdhan Gunawan <ilhammrg@gmail.com>
2023-02-16 19:47:53 +0700

    Controllers: get all users

---
+++ controllers/users.go
@@ -0,0 +1,55 @@
+package controllers
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"github.com/ilhamgunawan/lms-api-v2/models"
+)
+
+type GetUsersResponse struct {
+	Users        []models.UserAccount `json:"users"`
+	Offset       int                  `json:"offset"`
+	Limit        int                  `json:"limit"`
+	TotalCurrent int                  `json:"total_current"`
+	TotalAll     int                  `json:"total_all"`
+}
+
+func GetUsers(c *gin.Context) {
+	offset, err := strconv.Atoi(c.Query("offset"))
+
+	if err != nil {
+		offset = 0
+	}
+
+	limit, err := strconv.Atoi(c.Query("limit"))
+
+	if err != nil {
+		limit = 10
+	}
+
+	users, err := models.GetUsers(int(offset), int(limit))
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"message": err})
+		return
+	}
+
+	count, err := models.CountUsers()
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"message": err})
+		return
+	}
+
+	res := GetUsersResponse{
+		Users:        users,
+		Offset:       offset,
+		Limit:        limit,
+		TotalCurrent: len(users),
+		TotalAll:     int(count),
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": res})
+}
