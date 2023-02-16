c0d31d0ba75eae6377bf4d3132bb2f9e1a9c3544
Ilham Muhammad Ramdhan Gunawan <ilhammrg@gmail.com>
2023-02-16 19:47:18 +0700

    Models: get all users & count all users

--- models/user.go
+++ models/user.go
@@ -1,6 +1,10 @@
 package models
 
-import "github.com/ilhamgunawan/lms-api-v2/db"
+import (
+	"fmt"
+
+	"github.com/ilhamgunawan/lms-api-v2/db"
+)
 
 type User struct {
 	UserID       string `db:"user_id" json:"user_id"`
@@ -22,4 +26,33 @@ func GetUserByUsername(username string) (user User, err error) {
 	}
 
 	return user, nil
+}
+
+type UserAccount struct {
+	ID        string `db:"id" json:"id"`
+	FirstName string `db:"first_name" json:"first_name"`
+	LastName  string `db:"last_name" json:"last_name"`
+	BirthDate string `db:"date_of_birth" json:"date_of_birth"`
+	Gender    string `db:"gender" json:"gender"`
+}
+
+func GetUsers(offset, limit int) (users []UserAccount, err error) {
+	_, err = db.GetDB().Select(&users, "SELECT * FROM user_account OFFSET $1 LIMIT $2", offset, limit)
+
+	if err != nil {
+		fmt.Println("err", err)
+		return users, err
+	}
+
+	return users, nil
+}
+
+func CountUsers() (count int64, err error) {
+	count, err = db.GetDB().SelectInt("SELECT COUNT(*) FROM user_account")
+
+	if err != nil {
+		return count, err
+	}
+
+	return count, nil
 }
