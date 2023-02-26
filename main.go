c3117d9449d8c72da8fd9c436461dcc166df52f3
Ilham Muhammad Ramdhan Gunawan <ilhammrg@gmail.com>
2023-02-26 16:05:35 +0700

    Update CORS hanlder

--- main.go
+++ main.go
@@ -3,12 +3,15 @@ package main
 import (
 	"log"
 	"os"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/ilhamgunawan/lms-api-v2/db"
 	"github.com/ilhamgunawan/lms-api-v2/routes"
 	"github.com/joho/godotenv"
-	cors "github.com/rs/cors/wrapper/gin"
+
+	// cors "github.com/rs/cors/wrapper/gin"
+	"github.com/gin-contrib/cors"
 )
 
 func main() {
@@ -24,13 +27,21 @@ func main() {
 	r := gin.Default()
 
 	// Handle CORS
-	c := cors.New(cors.Options{
-		AllowedOrigins:       []string{"http://localhost:7001"},
-		AllowCredentials:     true,
-		AllowedMethods:       []string{"GET", "POST", "PUT", "DELETE", "HEAD"},
-		AllowedHeaders:       []string{"*", "Authorization"},
-		OptionsPassthrough:   true,
-		OptionsSuccessStatus: 204,
+	// c := cors.New(cors.Options{
+	// 	AllowedOrigins:       []string{"http://localhost:7001"},
+	// 	AllowCredentials:     true,
+	// 	AllowedMethods:       []string{"GET", "POST", "PUT", "DELETE", "HEAD"},
+	// 	AllowedHeaders:       []string{"*", "Authorization"},
+	// 	OptionsPassthrough:   true,
+	// 	OptionsSuccessStatus: 204,
+	// })
+	// r.Use(c)
+	c := cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:7001"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "HEAD"},
+		AllowHeaders:     []string{"*"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
 	})
 	r.Use(c)
 
